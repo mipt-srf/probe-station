6e4e4afd92eccf148198ded9721b7d4b5c70e33f
Sergey Ilev <ilev.sv@phystech.edu>
2024-02-06 14:40:34 +0300

    add plot_wakeup() function

--- pq_static.py
+++ pq_static.py
@@ -1,11 +1,14 @@
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import scipy  # type: ignore
 
-from probe_station.processor import get_half_cycle
+from probe_station.processor import get_cycle, get_half_cycle
 
 
+# TODO: move to OOP
 def get_polarization(df: pd.DataFrame, metadata: dict, big_pad: bool = False):
     wait_time, rump_time = metadata["Wait Time"], metadata["Rump time"]
     time_step = wait_time + rump_time
@@ -46,3 +49,28 @@ def get_default_wakeup(
         plt.legend()
         plt.gca().set_ylim(0, polarizations.max() * 1.05)
     return polarizations
+
+
+def plot_wakeup(
+    df: pd.DataFrame, metadata: dict, output_path: Path | str = ".", sample: str = ""
+):
+    repetitions = metadata["Repetition"]
+
+    transparencies = np.logspace(-0.4, -0.01, repetitions)
+    for i, alpha in enumerate(transparencies):
+        df_cycle = get_cycle(df, metadata, i, plot=False)
+        label = None
+        if i == 0 or i == repetitions - 1:
+            label = f"cycle #{i+1}"
+        plt.plot(
+            df_cycle["Voltages"],
+            df_cycle["DiffCurrent"],
+            alpha=alpha,
+            color="b",
+            label=label,
+        )
+    plt.xlabel("Voltage, V")
+    plt.ylabel("Current, A")
+    plt.title(f"I-V curve {sample}")
+    plt.legend()
+    plt.savefig(Path(output_path, sample + " wakeup (pq).png"))
