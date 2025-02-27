100297cb5b068b84f097757dbef15bd87a2daca3
Sergey Ilyev <ilev.sv@phystech.edu>
2025-02-27 13:41:10 +0300

    add src folder

--- probe_station/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Package for convenient operation with probe station datafiles."""
-
-from .dataset import Dataset
-
-__all__ = ["Dataset"]
---
+++ src/probe_station/__init__.py
@@ -0,0 +1,5 @@
+"""Package for convenient operation with probe station datafiles."""
+
+from .dataset import Dataset
+
+__all__ = ["Dataset"]
