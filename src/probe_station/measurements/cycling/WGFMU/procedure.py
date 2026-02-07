4ad32e448d54db788fdf33827ff65b305110f0b8
Sergey Ilyev <ilev.sv@phystech.edu>
2026-02-07 11:42:58 +0300

    add WGFMU cycling

---
+++ src/probe_station/measurements/cycling/WGFMU/procedure.py
@@ -0,0 +1,339 @@
+import logging
+import sys
+from enum import Enum
+from time import sleep
+
+import numpy as np
+import scipy
+from keysight_b1530a._bindings.config import WGFMUChannel
+from keysight_b1530a._bindings.errors import get_error_summary
+from keysight_b1530a._bindings.initialization import clear, close_session, initialize, open_session
+from keysight_b1530a.enums import (
+    WGFMUMeasureCurrentRange,
+)
+from keysight_b1530a.errors import WGFMUError
+from pymeasure.display.Qt import QtWidgets
+from pymeasure.display.widgets import LogWidget, PlotWidget
+from pymeasure.display.windows import ManagedWindowBase
+from pymeasure.experiment import (
+    BooleanParameter,
+    FloatParameter,
+    IntegerParameter,
+    ListParameter,
+    Procedure,
+)
+from PyQt5.QtCore import QLocale
+from waveform_generator import PulseSequence
+
+from probe_station.measurements.cycling.WGFMU.script import (
+    get_data,
+    get_sequence,
+    run,
+    set_waveform,
+)
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+
+def calculate_polarization(times, currents, pad_size_um):
+    charge = scipy.integrate.simpson(y=np.abs(currents), x=times)
+    area = (pad_size_um * 1e-4) ** 2
+    return charge / area * 1e6
+
+
+class SweepMode(Enum):
+    DEFAULT = "default"
+    PUND = "pund"
+
+
+class CyclingProcedure(Procedure):
+    mode = ListParameter("Mode", default=SweepMode.DEFAULT.name, choices=[e.name for e in SweepMode])
+
+    top = IntegerParameter("Top channel", default=2)
+    bottom = IntegerParameter("Bottom channel", default=1, group_by="enable_bottom")
+
+    voltage_top_first = FloatParameter("Top electrode voltage (first)", units="V", default=5.0)
+    voltage_top_second = FloatParameter("Top electrode voltage (second)", units="V", default=-5.0)
+
+    current_range = ListParameter(
+        "Current range",
+        default=WGFMUMeasureCurrentRange.RANGE_100_UA.name,
+        choices=[e.name for e in WGFMUMeasureCurrentRange],
+    )
+
+    enable_bottom = BooleanParameter("Enable bottom bias and measurement", default=False)
+
+    voltage_bottom_first = FloatParameter(
+        "Bottom electrode voltage (first)", units="V", default=-10.0, group_by="enable_bottom"
+    )
+    voltage_bottom_second = FloatParameter(
+        "Bottom electrode voltage (second)", units="V", default=10.0, group_by="enable_bottom"
+    )
+
+    pulse_time = FloatParameter("Pulse time", units="s", default=1e-5)
+
+    advanced_config = BooleanParameter("Advanced config", default=False)
+
+    steps = IntegerParameter("Steps per staircase", default=100, group_by="advanced_config")
+
+    repetitions = IntegerParameter("Number of cycles", default=1e3)
+
+    rise_to_hold_ratio = FloatParameter("Rise to hold time ratio", default=1, group_by="advanced_config")
+
+    calculate_polarization = BooleanParameter("Calculate Polarization", default=False)
+
+    pad_size = FloatParameter("Pad size", units="um", default=25, group_by="calculate_polarization")
+
+    DATA_COLUMNS = [
+        "Top electrode voltage",
+        "Top electrode Current",
+        "Time",
+        "Bottom electrode voltage",
+        "Bottom electrode current",
+        "Polarization current",
+        "Filtered Polarization current",
+    ]
+
+    def startup(self):
+        clear()
+        self.ch1 = WGFMUChannel.CH1
+        self.ch2 = WGFMUChannel.CH2
+        self.channels = [self.ch1, self.ch2]
+        open_session("USB1::0x0957::0x0001::0001::0::INSTR")
+        initialize()
+
+    def execute(self):
+        seq = get_sequence(
+            sequence_type=self.mode.lower(),
+            staircase_time=self.pulse_time,
+            max_voltage=self.voltage_top_first,
+            min_voltage=self.voltage_top_second,
+            steps=self.steps//2,
+            rise_to_hold_ratio=self.rise_to_hold_ratio,
+        )
+        if self.enable_bottom:
+            seq_bottom = get_sequence(
+                sequence_type=self.mode.lower(),
+                staircase_time=self.pulse_time,
+                max_voltage=self.voltage_bottom_first,
+                min_voltage=self.voltage_bottom_second,
+                steps=self.steps//2,
+                rise_to_hold_ratio=self.rise_to_hold_ratio,
+            )
+
+        if self.enable_bottom and max(abs(self.voltage_top_first - self.voltage_top_second), abs(self.voltage_bottom_first - self.voltage_bottom_second)) > 10:
+
+            seq_pu = PulseSequence(seq.pulses[:4])
+            seq_nd = PulseSequence(seq.pulses[4:])
+
+            seq_bottom_pu = PulseSequence(seq_bottom.pulses[:4])
+            seq_bottom_nd = PulseSequence(seq_bottom.pulses[4:])
+
+            set_waveform(
+                sequence=seq_pu,
+                repetitions=1,
+                channel=WGFMUChannel(self.top + 200),
+                measure_points=self.steps*2,
+                pattern_name="top_pu",
+            )
+            set_waveform(
+                    sequence=seq_bottom_pu,
+                    repetitions=1,
+                    channel=WGFMUChannel(self.bottom + 200),
+                    measure_points=self.steps*2,
+                    pattern_name="bottom_pu",
+                )
+            
+            try:
+                run(channels=[self.ch1, self.ch2], range=WGFMUMeasureCurrentRange[self.current_range])
+
+                times, voltages, currents = get_data(
+                    repetitions=1, ch=WGFMUChannel(self.top + 200), points=self.steps*2
+                )
+                times_bottom, voltages_bottom, currents_bottom = get_data(
+                        repetitions=1, ch=WGFMUChannel(self.bottom + 200), points=self.steps*4
+                    )
+
+            except WGFMUError:
+                print(get_error_summary())
+                clear()
+                close_session()
+
+            clear()
+
+            set_waveform(
+                sequence=seq_nd,
+                repetitions=1,
+                channel=WGFMUChannel(self.top + 200),
+                measure_points=self.steps*2,
+                pattern_name="top_nd",
+            )
+            set_waveform(
+                    sequence=seq_bottom_nd,
+                    repetitions=1,
+                    channel=WGFMUChannel(self.bottom + 200),
+                    measure_points=self.steps*2,
+                    pattern_name="bottom_nd",
+                )
+            
+            try:
+                run(channels=[self.ch1, self.ch2], range=WGFMUMeasureCurrentRange[self.current_range])
+
+                times_nd, voltages_nd, currents_nd = get_data(
+                    repetitions=1, ch=WGFMUChannel(self.top + 200), points=self.steps*2
+                )
+                times_bottom_nd, voltages_bottom_nd, currents_bottom_nd = get_data(
+                        repetitions=1, ch=WGFMUChannel(self.bottom + 200), points=self.steps*4
+                    )
+                
+
+            except WGFMUError:
+                print(get_error_summary())
+                clear()
+                close_session()
+
+            voltages = np.concatenate((voltages, voltages_nd))
+            currents = np.concatenate((currents, currents_nd))
+            times = np.concatenate((times, times_nd))
+            voltages_bottom = np.concatenate((voltages_bottom, voltages_bottom_nd))
+            currents_bottom = np.concatenate((currents_bottom, currents_bottom_nd))
+            times_bottom = np.concatenate((times_bottom, times_bottom_nd))
+
+            self.emit(
+                    "batch results",
+                    {
+                        "Top electrode voltage": voltages,
+                        "Top electrode Current": currents,
+                        "Time": times,
+                        "Bottom electrode voltage": voltages_bottom,
+                        "Bottom time": times_bottom,
+                        "Bottom electrode current": currents_bottom,
+                    },
+                )
+
+        else:
+            set_waveform(
+                sequence=seq,
+                repetitions=self.repetitions,
+                channel=WGFMUChannel(self.top + 200),
+                measure_points=self.steps*4,
+            )
+            if self.enable_bottom:
+                set_waveform(
+                    sequence=seq_bottom,
+                    repetitions=self.repetitions,
+                    channel=WGFMUChannel(self.bottom + 200),
+                    measure_points=self.steps*4,
+                )
+
+            try:
+                if self.enable_bottom:
+                    run(channels=[self.ch1, self.ch2], range=WGFMUMeasureCurrentRange[self.current_range])
+                else:
+                    run(channels=[WGFMUChannel(self.top + 200)], range=WGFMUMeasureCurrentRange[self.current_range])
+
+                # times, voltages, currents = get_data(
+                #     repetitions=1, ch=WGFMUChannel(self.top + 200), points=self.steps*4
+                # )
+                # if self.enable_bottom:
+                #     times_bottom, voltages_bottom, currents_bottom = get_data(
+                #         repetitions=1, ch=WGFMUChannel(self.bottom + 200), points=self.steps*4
+                #     )
+
+            except WGFMUError:
+                print(get_error_summary())
+                clear()
+                close_session()
+
+            # polarization_positive = np.concatenate(
+            #     (
+            #         currents[: len(currents) // 4] - currents[len(currents) // 4 : len(currents) // 2],
+            #         np.zeros(len(currents) // 4),
+            #     )
+            # )
+            # polarization_negative = np.concatenate(
+            #     (
+            #         currents[len(currents) // 2 : 3 * len(currents) // 4] - currents[3 * len(currents) // 4 :],
+            #         np.zeros(len(currents) // 4),
+            #     )
+            # )
+            # polarization_current = np.concatenate((polarization_positive, polarization_negative))
+            # filtered_polarization_current = scipy.ndimage.gaussian_filter1d(polarization_current, sigma=3)
+            # if self.enable_bottom:
+            #     self.emit(
+            #         "batch results",
+            #         {
+            #             "Top electrode voltage": voltages,
+            #             "Top electrode Current": currents,
+            #             "Time": times,
+            #             "Bottom electrode voltage": voltages_bottom,
+            #             "Bottom time": times_bottom,
+            #             "Bottom electrode current": currents_bottom,
+            #             "Polarization current": polarization_current,
+            #             "Filtered Polarization current": filtered_polarization_current,
+            #         },
+            #     )
+            # else:
+            #     self.emit(
+            #         "batch results",
+            #         {
+            #             "Top electrode voltage": voltages,
+            #             "Time": times,
+            #             "Top electrode Current": currents,
+            #             "Polarization current": polarization_current,
+            #             "Filtered Polarization current": filtered_polarization_current,
+            #         },
+            #     )
+            # if self.calculate_polarization:
+            #     polarization = calculate_polarization(times, filtered_polarization_current, self.pad_size)
+            #     log.info("Polarization (Pr): %s", polarization)
+
+        close_session()
+
+
+class MainWindow(ManagedWindowBase):
+    def __init__(self):
+        widget_list = (
+            PlotWidget("Results Graph", CyclingProcedure.DATA_COLUMNS),
+            LogWidget("Experiment Log"),
+        )
+
+        settings = [
+            "mode",
+            "pulse_time",
+            "voltage_top_first",
+            "voltage_top_second",
+            "repetitions",
+            "top",
+            "current_range",
+            "enable_bottom",
+            "voltage_bottom_first",
+            "voltage_bottom_second",
+            "bottom",
+            "advanced_config",
+            "steps",
+            "rise_to_hold_ratio",
+            "calculate_polarization",
+            "pad_size",
+        ]
+
+        super().__init__(
+            procedure_class=CyclingProcedure,
+            inputs=settings,
+            displays=settings,
+            widget_list=widget_list,
+        )
+        logging.getLogger().addHandler(widget_list[1].handler)
+        log.setLevel(self.log_level)
+        log.info("ManagedWindow connected to logging")
+        self.setWindowTitle(f"{self.procedure_class.__name__}")
+        self.store_measurement = False
+
+
+if __name__ == "__main__":
+    QLocale.setDefault(QLocale(QLocale.English, QLocale.UnitedStates))
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
---
+++ src/probe_station/measurements/cycling/WGFMU/script.py
@@ -0,0 +1,136 @@
+import numpy as np
+from keysight_b1530a._bindings.config import WGFMUChannel
+from keysight_b1530a._bindings.configuration import set_operation_mode
+from keysight_b1530a._bindings.data_retrieval import get_measurement_data, get_voltage_data
+from keysight_b1530a._bindings.errors import get_error_summary
+from keysight_b1530a._bindings.event_setup import set_measure_event
+from keysight_b1530a._bindings.initialization import clear, close_session, open_session
+from keysight_b1530a._bindings.measurement import (
+    connect,
+    execute,
+    set_measure_current_range,
+    set_measure_mode,
+    wait_until_completed,
+)
+from keysight_b1530a._bindings.pattern_setup import (
+    add_vectors,
+    create_pattern,
+)
+from keysight_b1530a._bindings.sequence_setup import add_sequence
+from keysight_b1530a.enums import (
+    WGFMUMeasureCurrentRange,
+    WGFMUMeasureEvent,
+    WGFMUMeasureMode,
+    WGFMUOperationMode,
+)
+from keysight_b1530a.errors import WGFMUError
+from waveform_generator import PulseSequence, StaircaseSweep, TrapezoidalPulse
+
+
+def get_sequence(
+    sequence_type="pund", staircase_time=1e-4, steps=50, max_voltage=4, min_voltage=-4, rise_to_hold_ratio=0.01
+):
+    time_step = staircase_time / steps / (1 + rise_to_hold_ratio)
+    edge_time = time_step * rise_to_hold_ratio
+
+    positive_rise = StaircaseSweep(end_voltage=max_voltage, time_step=time_step, steps=steps, edge_time=edge_time)
+    positive_fall = StaircaseSweep(
+        start_voltage=max_voltage, end_voltage=0, time_step=time_step, steps=steps, edge_time=edge_time
+    )
+    positive = [positive_rise, positive_fall]
+
+    negative_rise = StaircaseSweep(end_voltage=min_voltage, time_step=time_step, steps=steps, edge_time=edge_time)
+    negative_fall = StaircaseSweep(
+        start_voltage=min_voltage, end_voltage=0, time_step=time_step, steps=steps, edge_time=edge_time
+    )
+    negative = [negative_rise, negative_fall]
+
+    trapezoidal = TrapezoidalPulse(
+        amplitude=0.0, pulse_width=edge_time, rise_time=10 * edge_time, fall_time=edge_time
+    )  # fictional pulse to add delay at the end (otherwise last points are missed)
+
+    if sequence_type == "pund":
+        # sequence = PulseSequence(positive * 2 + negative * 2 + [trapezoidal])
+        sequence = PulseSequence(positive * 2 + negative * 2)
+    else:  # default
+        sequence = PulseSequence(positive + negative)
+
+    # sequence.pulses[0].delay = time_step + edge_time
+    return sequence
+
+
+def set_waveform(
+    sequence,
+    repetitions=1,
+    channel=WGFMUChannel.CH1,
+    measure=False,
+    measure_points=1600,
+    pattern_name="sequence",
+):
+    pattern_name += f"_{channel.name.lower()}"
+    create_pattern(pattern_name, sequence.pulses[0].dc_bias)
+    times, voltages = sequence.to_vectors()
+    print(f"2*steps in full PUND sequence = {len(voltages)}")
+    add_vectors(pattern_name, times, voltages)
+    seq_time = sequence.total_duration
+    if measure:
+        set_measure_event(
+            pattern_name=pattern_name,
+            event_name="event",
+            points=measure_points,
+            interval=seq_time / measure_points *1.02,
+            average=seq_time / measure_points,
+            mode=WGFMUMeasureEvent.AVERAGED,
+        )
+    add_sequence(pattern_name, repetitions, channel=channel)
+
+
+def run(channels=[WGFMUChannel.CH2], mode=WGFMUOperationMode.FASTIV, range=WGFMUMeasureCurrentRange.RANGE_1_UA):
+    for channel in channels:
+        set_operation_mode(channel, mode)
+        # set_measure_mode(channel, WGFMUMeasureMode.CURRENT)
+        # set_measure_current_range(channel, range)
+        connect(channel)
+    execute()
+    wait_until_completed()
+
+
+def get_data(repetitions, ch=WGFMUChannel.CH2, points=50):
+    times, currents = get_measurement_data(ch)
+    voltages = get_voltage_data(ch)
+    print(f"Data length = {len(voltages)}, {len(currents)}")
+    # # drop all except last rep
+    # times = np.split(np.array(times), repetitions)[-1]
+    # currents = np.split(np.array(currents), repetitions)[-1]
+    # voltages = np.split(np.array(voltages), repetitions)[-1]
+    # print(len(voltages), points, len(voltages) // points)
+
+    # times = np.mean(times.reshape(-1, len(voltages) // points), axis=1)
+    # currents = np.mean(currents.reshape(-1, len(voltages) // points), axis=1)
+    # voltages = np.mean(voltages.reshape(-1, len(voltages) // points), axis=1)
+    # print(len(voltages))
+
+    return times, voltages, currents
+
+    # plt.plot(voltages, currents)
+    # plt.figure()
+    # plt.plot(times, currents)
+    # plt.show()
+
+
+if __name__ == "__main__":
+    repetitions = 2
+    clear()
+    ch1 = WGFMUChannel.CH1
+    ch2 = WGFMUChannel.CH2
+    open_session()
+    pund = get_sequence(sequence_type="pund")
+    print(pund.to_vectors()[0], "\n\n\n", pund.to_vectors()[1])
+    close_session()
+    # set_waveform(sequence=pund, repetitions=repetitions, channel=ch2)
+    # set_waveform(sequence=-pund, repetitions=repetitions, channel=ch1)
+    # try:
+    #     run(channels=[ch1, ch2])
+    # except WGFMUError:
+    #     print(get_error_summary())
+    #     clear()
