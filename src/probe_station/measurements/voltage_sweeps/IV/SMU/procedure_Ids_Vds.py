2cc7dd3c4e05c12f407fae2952dd3be1ae9d2be7
Sergey Ilyev <ilev.sv@phystech.edu>
2026-02-07 11:45:43 +0300

    add Ids(Vds) measurement

---
+++ src/probe_station/measurements/voltage_sweeps/IV/SMU/procedure_Ids_Vds.py
@@ -0,0 +1,108 @@
+import logging
+import sys
+
+import numpy as np
+from keysight_b1530a._bindings.initialization import open_session
+from pymeasure.display.Qt import QtWidgets
+from pymeasure.display.widgets import LogWidget, PlotWidget
+from pymeasure.display.windows import ManagedWindowBase
+from pymeasure.experiment import BooleanParameter, FloatParameter, IntegerParameter, Procedure
+from PyQt5.QtCore import QLocale
+
+from probe_station.measurements.common import connect_instrument
+from probe_station.measurements.voltage_sweeps.IV.SMU.script_Ids_Vds import get_data, run
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+
+class IvSweepProcedure(Procedure):
+    first_voltage = FloatParameter("First voltage", units="V", default=-3)
+    second_voltage = FloatParameter("Second voltage", units="V", default=3)
+    gate_voltage = FloatParameter("Gate voltage", units="V", default=0)
+    source_channel = IntegerParameter("Source channel", default=4)
+    drain_channel = IntegerParameter("Drain channel", default=3)
+    gate_channel = IntegerParameter("Gate channel", default=1)
+    average = IntegerParameter("Intergration coefficient", default=127, minimum=1, maximum=127)
+    advanced_config = BooleanParameter("Advanced config", default=False)
+    steps = IntegerParameter("Steps", default=100, group_by="advanced_config")
+    mode = IntegerParameter("Mode", default=1, group_by="advanced_config")
+    # compliance = FloatParameter("Current compliance", units="A", default=0.1, group_by="advanced_config")
+
+    DATA_COLUMNS = ["Voltage", "Source electrode current", "Time"]
+
+    def startup(self):
+        self.b1500 = connect_instrument(timeout=60000, reset=False)
+        open_session()
+
+    def execute(self):
+        log.info(f"Starting the {self.__class__}")
+
+        run(
+            self.b1500,
+            self.first_voltage,
+            self.second_voltage,
+            self.steps,
+            top=self.source_channel,
+            # current_comp=self.compliance,
+            average=self.average,
+            mode=self.mode,
+            gate=self.gate_channel,
+            gate_voltage=self.gate_voltage
+        )
+        times, voltages, currents = get_data(self.b1500)
+        # print(f"len(times) = {len(times), len(voltages), len(currents)}")
+        # print(voltages[:20])
+
+        self.emit(
+            "batch results",
+            {"Time": times, "Voltage": voltages, "Source electrode current": np.abs(currents)},
+        )
+
+        times, voltages, currents = get_data(self.b1500)
+        # print(f"len(times) = {len(times), len(voltages), len(currents)}")
+        # print(currents[:20])
+
+        self.emit(
+            "batch results",
+            {"Time": times, "Voltage": voltages, "Source electrode current": np.abs(currents)},
+        )
+
+
+class MainWindow(ManagedWindowBase):
+    def __init__(self):
+        widget_list = (
+            PlotWidget("Results Graph", IvSweepProcedure.DATA_COLUMNS),
+            LogWidget("Experiment Log"),
+        )
+        settings = [
+            "first_voltage",
+            "second_voltage",
+            "source_channel",
+            "drain_channel",
+            "average",
+            "advanced_config",
+            "steps",
+            "mode",
+            "gate_channel",
+            "gate_voltage"
+            # "compliance",
+        ]
+        super().__init__(
+            procedure_class=IvSweepProcedure,
+            inputs=settings,
+            displays=settings,
+            widget_list=widget_list,
+        )
+        logging.getLogger().addHandler(widget_list[1].handler)
+        log.setLevel(self.log_level)
+        log.info("ManagedWindow connected to logging")
+        self.setWindowTitle(f"{self.procedure_class.__name__}")
+
+
+if __name__ == "__main__":
+    QLocale.setDefault(QLocale(QLocale.English, QLocale.UnitedStates))
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
---
+++ src/probe_station/measurements/voltage_sweeps/IV/SMU/script_Ids_Vds.py
@@ -0,0 +1,122 @@
+from keysight_b1530a._bindings.initialization import close_session, open_session
+from pymeasure.instruments.agilent.agilentB1500 import (
+    ADCMode,
+    ADCType,
+    AgilentB1500,
+    MeasMode,
+    MeasOpMode,
+    SweepMode,
+)
+
+from probe_station.measurements.common import (
+    RSU,
+    RSUOutputMode,
+    connect_instrument,
+    get_smu_by_number,
+    parse_data,
+    setup_rsu_output,
+)
+
+
+def run(b1500: AgilentB1500, start, end, steps, average=127, top=4, bottom=3, mode=1, gate=1, gate_voltage=1):
+    # b1500.reset()
+    setup_rsu_output(b1500, rsu=RSU.RSU1, mode=RSUOutputMode.SMU)
+    setup_rsu_output(b1500, rsu=RSU.RSU2, mode=RSUOutputMode.SMU)
+
+    smu = get_smu_by_number(b1500, top)
+    smu.enable()
+    smu.force("voltage", 0, 0, 1e-1)
+
+    smu_bottom = get_smu_by_number(b1500, bottom)
+    smu_bottom.enable()
+    smu_bottom.force("voltage", 0, 0, 1e-1)
+
+    gate_smu = get_smu_by_number(b1500, gate)
+    gate_smu.enable()
+    gate_smu.force("voltage", 0, gate_voltage, 20e-3)
+
+    # b1500.write("SSP 9,3")
+    # b1500.adc_auto_zero = True
+    b1500.time_stamp = True
+    b1500.adc_averaging(10)
+    b1500.meas_mode(MeasMode.STAIRCASE_SWEEP, smu)  # drain
+    smu.meas_op_mode = MeasOpMode.CURRENT
+    smu.meas_range_current = 0
+    smu.adc_type = 1
+
+    b1500.adc_setup(ADCType.HRADC, ADCMode.MANUAL, average)
+    # smu.sweep_timing()
+
+    if smu.channel in [3, 4]:
+        max_voltage = max(abs(start), abs(end))
+        if max_voltage <= 20:
+            compliance = 100e-3
+        elif 20 < max_voltage <= 40:
+            compliance = 50e-3
+        elif 40 < max_voltage <= 100:
+            compliance = 20e-3
+        else:
+            raise ValueError(f"Voltages higher than 100 V are not suported by {smu.name}")
+    else:
+        compliance = 20e-3  # temp fix for other smus
+
+    if mode == 1:
+        smu.staircase_sweep_source(
+            source_type="Voltage",
+            mode=SweepMode.LINEAR_DOUBLE,
+            source_range=0,
+            start=start,
+            stop=end,
+            steps=steps,
+            comp=compliance,
+            # Pcomp=0.03,
+        )
+    elif mode == 2:
+        smu.staircase_sweep_source(
+            source_type="Voltage",
+            mode=SweepMode.LINEAR_DOUBLE,
+            source_range=0,
+            start=0,
+            stop=start,
+            steps=steps // 2,  # fix steps num
+            comp=compliance,
+            # Pcomp=0.03,
+        )
+
+    b1500.clear_timer()
+
+    b1500.send_trigger()
+
+    if mode == 2:
+        smu.staircase_sweep_source(
+            source_type="Voltage",
+            mode=SweepMode.LINEAR_DOUBLE,
+            source_range=0,
+            start=0,
+            stop=end,  # fix steps
+            steps=steps // 2,
+            comp=compliance,
+            # Pcomp=0.03,
+        )
+
+    b1500.send_trigger()
+
+    smu.force("voltage", 0, 0)
+
+
+def get_data(b1500: AgilentB1500):
+    data = parse_data(b1500.read())
+
+    times = data[::3]
+    currents = data[1::3]
+    voltages = data[2::3]
+
+    return times, voltages, currents
+
+
+if __name__ == "__main__":
+    b1500 = connect_instrument(reset=True)
+    open_session()
+    run(b1500, start=-3, end=3, steps=100, top=4)
+    get_data(b1500)
+    close_session()
