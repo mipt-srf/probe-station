5b887fc12be22e64c39843e6de36c2d5b6ab8736
Sergey Ilyev <ilev.sv@phystech.edu>
2026-03-04 19:25:36 +0300

    add missing WGFMU non-channel functions

--- src/probe_station/measurements/b1500.py
+++ src/probe_station/measurements/b1500.py
@@ -1,9 +1,20 @@
+from functools import wraps
+
 from keysight_b1530a import (
     WGFMU,
+    add_vector,
+    add_vectors,
+    clear,
     close_session,
+    create_pattern,
+    execute,
     get_channel_ids,
+    initialize,
     open_session,
+    set_measure_event,
+    wait_until_completed,
 )
+from keysight_b1530a.enums import WGFMUMeasureEvent
 from pymeasure.instruments.agilent.agilentB1500 import AgilentB1500
 
 
@@ -40,6 +51,45 @@ class B1500(AgilentB1500):
             close_session()  # Note: will cause the error if the session was closed already from outside
             self._wgfmu_session_opened = False
 
+    @wraps(get_channel_ids)
     def query_wgfmu_channels(self):
         """Query the available WGFMU channel IDs."""
         return get_channel_ids()
+
+    @wraps(execute)
+    def run_wgfmu_measurement(self):
+        execute()
+        wait_until_completed()
+
+    @wraps(create_pattern)
+    def create_wgfmu_pattern(self, name: str, start_voltage: float):
+        return create_pattern(name, start_voltage)
+
+    @wraps(add_vector)
+    def add_vector_to_wgfmu_pattern(self, pattern_name: str, voltage: float, duration: float):
+        add_vector(pattern_name, voltage, duration)
+
+    @wraps(add_vectors)
+    def add_vectors_to_wgfmu_pattern(self, pattern_name: str, voltages: list[float], durations: list[float]):
+        add_vectors(pattern_name, voltages, durations)
+
+    @wraps(set_measure_event)
+    def set_wgfmu_measure_event(
+        self,
+        pattern_name: str,
+        event_name: str,
+        points: int,
+        interval: float,
+        average: float,
+        mode: WGFMUMeasureEvent = WGFMUMeasureEvent.AVERAGED,
+        start_time: float = 0.0,
+    ):
+        set_measure_event(pattern_name, event_name, points, interval, average, mode, start_time)
+
+    @wraps(initialize)
+    def initialize_wgfmu(self):
+        initialize()
+
+    @wraps(clear)
+    def clear_wgfmu(self):
+        clear()
